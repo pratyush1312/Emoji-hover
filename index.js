06eff867c38462726cfd521d7ee82645bfeacd8c
Pratyush <pratyushpandey730@gmail.com>
2021-03-12 01:18:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+let emoji = document.getElementById("emoji");
+let emojiArr = [
+  "😀",
+  "😁",
+  "😂",
+  "🤣",
+  "😃",
+  "😄",
+  "😅",
+  "😆",
+  "😉",
+  "😊",
+  "😋",
+  "😍",
+  "😎",
+  "😘",
+  "🥰",
+  "😗",
+  "🤨",
+  "🤔",
+  "🤩",
+  "🙂",
+  "🤗",
+  "🙂",
+  "😚",
+  "😙",
+];
+
+let incr = 0;
+
+emoji.innerHTML = emojiArr[incr];
+incr++;
+
+emoji.addEventListener("mouseover", function () {
+  if (incr == emojiArr.length) {
+    incr = 0;
+  } else {
+    emoji.innerHTML = emojiArr[incr];
+    incr++;
+  }
+});
